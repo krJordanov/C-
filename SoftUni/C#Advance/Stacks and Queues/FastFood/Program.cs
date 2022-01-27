765df3572c20ad0a1b480aa4d9f3cc328d687892
krJordanov <96188286+krJordanov@users.noreply.github.com>
2022-01-27 17:53:44 +0200

    Add files via upload

---
+++ SoftUni/C#Advance/Stacks and Queues/FastFood/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   int qtyFood = int.Parse(Console.ReadLine());
+            //  Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            //  int biggestOrder = orders.Max();
+            //  int count = orders.Count;
+            //  for (int i = 0; i < count; i++)
+            //  {
+               // if (qtyFood >= orders.Peek())
+              //  {
+              //   qtyFood -= orders.Dequeue();
+              //   continue;
+             //   }
+
+                //break;
+            //  }
+            //  Console.WriteLine(biggestOrder);
+
+           //   Console.WriteLine(orders.Count <= 0 ? "Orders complete" : $"Orders left: {string.Join(" ", orders)}");
+
+
+            int quantity = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> queueOfOrders = new Queue<int>();
+
+            foreach (var order in orders)
+            {
+                queueOfOrders.Enqueue(order);
+            }
+
+            int maxOrder = int.MinValue;
+
+            while (quantity > 0 && queueOfOrders.Count > 0)
+            {
+                int currentOrder = queueOfOrders.Peek();
+
+                if (currentOrder > maxOrder)
+                {
+                    maxOrder = currentOrder;
+                }
+
+                if (quantity - currentOrder >= 0)
+                {
+                    quantity -= currentOrder;
+                    queueOfOrders.Dequeue();
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(maxOrder);
+            if (queueOfOrders.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" " , queueOfOrders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
---
+++ SoftUni/C#Advance/Stacks and Queues/SongsQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+            string command = Console.ReadLine();
+
+            while (songs.Count > 0)
+            {
+                int indexSeparator = command.IndexOf(' ');
+                switch (indexSeparator == -1 ? command : command.Substring(0, indexSeparator))
+                {
+                    case "Play":
+                        songs.Dequeue();
+                        break;
+                    case "Add":
+                        string songsAdd = command.Substring(indexSeparator + 1);
+                        if (!songs.Contains(songsAdd))
+                        {
+                            songs.Enqueue(songsAdd);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{songsAdd} is already contained!");
+                        }
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songs));
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
